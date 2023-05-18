fbd5adf3157789249e15acc3aa286b806ee9c97a
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-18 12:29:36 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,12 @@
 
 class Dog:
     # Class body goes here
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+        print("The dog is sitting.")
 
     #Instance method definition
-    pass
+fido = Dog()
+fido.bark()
+    # pass
